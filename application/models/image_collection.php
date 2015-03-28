cba54c9c9edb630f115f11307c9888f477f235c8
Mihai Vinaga <mihai.vinaga@amoma.com>
2015-03-28 15:37:34 +0200

    Added save and get methods

--- application/models/image_collection.php
+++ application/models/image_collection.php
@@ -3,9 +3,11 @@ class image_collection extends kms_item_collection {
 	
 	public function __construct() {
 		parent::__construct();
-		$this -> _load_crud_data('<? echo $model; ?>');
+		$this -> _load_crud_data('image');
 	}
+
+    public function get_by_id($id) {
+        return $this->get_one(array('id' => $id));
+    }
 	
 }
-
-?>
--- application/models/item_collection.php
+++ application/models/item_collection.php
@@ -3,9 +3,30 @@ class item_collection extends kms_item_collection {
 	
 	public function __construct() {
 		parent::__construct();
-		$this -> _load_crud_data('<? echo $model; ?>');
+		$this -> _load_crud_data('item');
+        $this->load->model('image_collection');
 	}
-	
-}
 
-?>
+    public function get_by_id($id) {
+        return $this->get_one(array('id' => $id));
+    }
+
+    public function save($data, $id = 0, $action = null) {
+        $item = parent::save($data, $id, $action);
+        $item = $this->get_by_id($item['id']);
+
+        if (isset($data['images'])) {
+            foreach ((array)$data['images'] as $image) {
+                $image_data = [
+                    'item_id' => $item['id'],
+                    'path' => $image['path'],
+                    'is_selected' => $image['is_selected']
+                ];
+                $this->image_collection->save($image_data);
+            }
+        }
+        return $item;
+    }
+
+
+}
